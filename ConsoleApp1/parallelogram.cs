ddd540968846334f2dec2fd53cb44d0410b9a31f
schnaya <76622822+schnaya@users.noreply.github.com>
2022-03-21 23:20:19 +0300

    Update parallelogram.cs

--- ConsoleApp1/parallelogram.cs
+++ ConsoleApp1/parallelogram.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
 {
     class parallelogram: Line
     {
-        public virtual void Func()
+        public override void figuredraw()
         {
 
          Console.WriteLine("parallelogram(10, 10, 50, 100)");
